b842622fd2716aa86e74a0820831069b0bf7ab82
ahazlett21 <ahazlett21@gmail.com>
2021-12-02 09:50:26 -0500

    jurhjve

--- CreativeCoding/P5/square loops/sketch.js
+++ CreativeCoding/P5/square loops/sketch.js
@@ -6,14 +6,14 @@ function setup() {//runs once
 
 function draw() {//runs in a loop
   background(220);
-  
+
   translate (60,60);
-  
+
  //loop to create rows in the y direction
   for(var y = 0; y < 600; y = y + 300){
-    
-    
-  
+
+
+
   for(var x = 0; x < 600; x = x + 300){ //loop to create a row of squares in the x direction
 
 
@@ -22,8 +22,8 @@ function draw() {//runs in a loop
        x+300, y+300,
        x,y+300)
   }
-    
+
     }
-  
+
   noLoop ();
 }

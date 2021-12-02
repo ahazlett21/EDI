5166ff720bc489dbdf85566249616233053c88b2
ahazlett21 <ahazlett21@gmail.com>
2021-12-02 09:51:57 -0500

    k

--- CreativeCoding/P5/tuningwithvariables/sketch.js
+++ CreativeCoding/P5/tuningwithvariables/sketch.js
@@ -1,3 +1,13 @@
+let gif;
+
+function preload (){
+  gif=loadImage ('assets/AiBG.gif');
+
+
+
+}
+
+
 function setup() { //runs once
   createCanvas(windowWidth, windowWidth);
   fill('magenta');
@@ -11,11 +21,15 @@ var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < windowWidth; y = y + sideLen) {//loop to create rows in the y direction
      for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
-  quad(x, y,//this makes a 300x300 pixel square
-    x + sideLen, y,
-    x + sideLen, y + sideLen,
-    x, y + sideLen);
+image (gif,x,y);
   }
+  function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
   }
 
 }
+// quad(x, y,//this makes a 300x300 pixel square
+//   x + sideLen, y,
+//   x + sideLen, y + sideLen,
+//   x, y + sideLen);
